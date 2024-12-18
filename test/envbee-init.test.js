192874ce9fa81c2d85f20be11a83b5c68049e992
Gustavo Spadari <gspadari@gmail.com>
2024-12-17 23:03:22 -0300

    add get variables pagination with unit test

--- test/envbee-init.test.js
+++ test/envbee-init.test.js
@@ -20,7 +20,7 @@ test.afterEach(() => {
   global.fetch = originalFetch;
 });
 
-test("envbee-init - Missing key and / or secret", function(t) {
+test("envbee-init - Missing key and / or secret", function (t) {
   const error1 = t.throws(() => envbeeInit());
   t.like(error1, { message: MISSING_KEY_AND_SECRET });
   const error2 = t.throws(() => envbeeInit({}));
@@ -37,14 +37,14 @@ test("envbee-init - Missing key and / or secret", function(t) {
   t.like(error7, { message: MISSING_KEY_AND_SECRET });
 });
 
-test("envbee-init - Valid parameters", function(t) {
+test("envbee-init - Valid parameters", function (t) {
   const envbee = envbeeInit({ apiURL, key, secret });
   t.not(envbee, null);
   t.not(envbee, undefined);
   t.is(typeof envbee, "object");
 });
 
-test("envbee-init - Get all variables (invalid credentials)", async function(t) {
+test("envbee-init - Get all variables (invalid credentials)", async function (t) {
   const envbee = envbeeInit({ apiURL, key, secret: "INVALID_SECRET" });
 
   global.fetch = async (url) => {
@@ -67,7 +67,7 @@ test("envbee-init - Get all variables (invalid credentials)", async function(t) 
   }
 });
 
-test("envbee-init - Get all variables", async function(t) {
+test("envbee-init - Get all variables", async function (t) {
   const envbee = envbeeInit({ apiURL, key, secret });
 
   global.fetch = async (url) => {
@@ -97,9 +97,40 @@ test("envbee-init - Get all variables", async function(t) {
   t.is(typeof variable1.value, "string");
 });
 
-test.todo("envbee-init - Get all variables (with pagination)");
+test("envbee-init - Get all variables (with pagination)", async function (t) {
+  const envbee = envbeeInit({ apiURL, key, secret });
 
-test("envbee-init - Get variable value", async function(t) {
+  global.fetch = async (url) => {
+    return {
+      ok: true,
+      status: 200,
+      json: () =>
+        Promise.resolve({
+          metadata: { limit: 1, offset: 10, total: 100 },
+          data: [{ name: "VAR1", value: "VALUE1" }]
+        })
+    };
+  };
+
+  const { data, metadata } = await envbee.getAllVariables(10, 1);
+
+  t.is(typeof metadata.limit, "number");
+  t.is(typeof metadata.offset, "number");
+  t.is(typeof metadata.total, "number");
+  t.true(metadata.total > 0);
+
+  t.true(Array.isArray(data));
+  t.true(data.length > 0);
+
+  const [variable1] = data;
+  t.is(typeof variable1.name, "string");
+  t.is(typeof variable1.value, "string");
+
+  t.is("VAR1", variable1.name);
+  t.is("VALUE1", variable1.value);
+});
+
+test("envbee-init - Get variable value", async function (t) {
   const envbee = envbeeInit({ apiURL, key, secret });
 
   global.fetch = async (url) => {
